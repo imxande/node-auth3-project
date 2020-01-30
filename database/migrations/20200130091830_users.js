155d844b84764fcf280ef7f45c46ba2ec073fad0
imxande <alerodriguezlarosa@gmail.com>
2020-01-30 09:24:04 -0500

    created knexfile and table

---
+++ database/migrations/20200130091830_users.js
@@ -0,0 +1,21 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('users', users => {
+      users.increments();
+    
+    // username
+      users
+        .string('username', 128)
+        .notNullable()
+        .unique();
+    // password  
+      users.string('password', 128).notNullable();
+    
+    // deparment
+      users.string('department', 128)
+    });
+  };
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('users');
+  };
+  
