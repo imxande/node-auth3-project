5789d1b11b06743c3c3a04a87146db0d2e348c01
imxande <alerodriguezlarosa@gmail.com>
2020-01-30 11:14:39 -0500

    added all users methods

--- users/users-model.js
+++ users/users-model.js
@@ -3,10 +3,32 @@ const db = require('../database/dbConfig.js');
 
 // created methods here
 module.exports = {
-    find
+    find,
+    add,
+    findBy,
+    findById
 }
 
 // function find implementation
 function find(){
     return db('users').select('id', 'username', 'password');
 }
+
+// add method implementation
+async function add(user) {
+    const [id] = await db('users').insert(user);
+  
+    return findById(id);
+  }
+
+  // findby implementation
+  function findBy(filter) {
+    return db('users').where(filter);
+  }
+
+  // find by id implementation
+  function findById(id) {
+    return db('users')
+      .where({ id })
+      .first();
+  }
